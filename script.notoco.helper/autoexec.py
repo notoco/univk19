e70bd7a45653ce3e2ff58e949acba67b43db2e86
notoco <36659110+notoco@users.noreply.github.com>
2022-08-22 21:09:11 +0200

    repair

--- script.notoco.helper/autoexec.py
+++ script.notoco.helper/autoexec.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import control
+#import control
 
-state = control.get_setting('state')
+#state = control.get_setting('state')
 
-if state == 'true':
-    control.ambilight_turn_on()
-else:
-    control.ambilight_turn_off()
+#if state == 'true':
+#    control.ambilight_turn_on()
+#else:
+#    control.ambilight_turn_off()
