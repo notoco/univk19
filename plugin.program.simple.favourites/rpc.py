08522fff14575d2e1d7b9f36da8c287ce7b8dae1
notoco <36659110+notoco@users.noreply.github.com>
2022-10-17 14:45:03 +0200

    up

--- plugin.program.simple.favourites/rpc.py
+++ plugin.program.simple.favourites/rpc.py
@@ -1,16 +1,19 @@
 import sys
+
 PY3 = sys.version_info.major >= 3
 
 if PY3:
     from builtins import str, object
 
 import json
-#from xbmcswift2 import xbmc
+# from xbmcswift2 import xbmc
 import xbmc
+
 
 class RPCType(type):
     def __getattr__(cls, category):
         return Category(category)
+
 
 if PY3:
     class RPC(object, metaclass=RPCType):
@@ -18,14 +21,15 @@ if PY3:
 else:
     class RPC(object):
         __metaclass__ = RPCType
-    
+
+
 class Category(object):
     def __init__(self, name):
         self.name = name
-        
+
     def __str__(self):
         return self.name.title().replace("_", "")
-    
+
     def __getattr__(self, method):
         return Method(self, method)
 
@@ -34,33 +38,29 @@ class Method(object):
     def __init__(self, category, name):
         self.category = category
         self.name = name
-            
+
     def __str__(self):
         return self.name.title().replace("_", "")
-        
+
     def __call__(self, **kwargs):
         method = "%s.%s" % (str(self.category), str(self))
         query = {"method": method, "params": kwargs}
         return json_query(query)
-        
+
+
 class RPCError(Exception):
     pass
-    
+
+
 def json_query(query):
     if not "jsonrpc" in query:
         query["jsonrpc"] = "2.0"
     if not "id" in query:
         query["id"] = 1
-    
+
     xbmc_request = json.dumps(query)
-    if PY3:
-        response = json.loads(xbmc.executeJSONRPC(xbmc_request))
-    else:
-        raw = xbmc.executeJSONRPC(xbmc_request)
-        clean = unicode(raw, 'utf-8', errors='ignore')
-        response = json.loads(clean)
+    response = json.loads(xbmc.executeJSONRPC(xbmc_request))
+
     if "error" in response:
         raise RPCError(response["error"])
     return response.get('result', response)
-
-    
