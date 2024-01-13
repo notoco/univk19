773bf385d61ee59e8f01f2ebd80f5ce9caa36fb1
notoco <36659110+notoco@users.noreply.github.com>
2024-01-13 08:53:33 +0100

    up

--- script.audio.profiles/default.py
+++
@@ -1,5 +0,0 @@
-
-from resources.lib.audioprofiles import apManual
-
-if (__name__ == "__main__"):
-    apManual()
--- script.audio.profiles/service.py
+++
@@ -1,5 +0,0 @@
-
-from resources.lib.audioprofiles import apMonitor
-
-if ( __name__ == "__main__" ):
-    apMonitor()
