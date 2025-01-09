1604390543fc8a52d80e1b2fb0f88783e2a5e884
notoco <36659110+notoco@users.noreply.github.com>
2025-01-09 18:36:55 +0100

    Reapply "Merge branch 'master' of https://github.com/notoco/univk19"
    This reverts commit fc60f17b6f8979d7764ea1e2fec7fa43d8bf65b2.

--- script.notoco.helper/control.py
+++ script.notoco.helper/control.py
@@ -1,54 +1,52 @@
-# -*- coding: utf-8 -*-
 import xbmcaddon
 import xbmcgui
 import requests
-import xbmc
-import time
 
-
-headers = {
-    'Content-Type': 'application/json',
-}
+# Konfiguracja
+headers = {'Content-Type': 'application/json'}
 addon = xbmcaddon.Addon()
-state = addon.getSetting('state')
 icon = addon.getAddonInfo('icon')
 
-def ambilight_turn_off():
-    data = {"command":"componentstate", "componentstate":{"component": "LEDDEVICE", "state": False }, "tan":1}
-    response = requests.post('http://127.0.0.1:8090/json-rpc', headers=headers, json=data)
-    addon.setSetting('state', 'false')
+def send_request(command, payload):
+    """Wysyła żądanie do serwera Ambilight."""
+    url = 'http://127.0.0.1:8090/json-rpc'
+    data = {"command": command, **payload, "tan": 1}
+    requests.post(url, headers=headers, json=data)
 
-def ambilight_turn_on():
-    data = {"command":"componentstate", "componentstate":{"component": "LEDDEVICE", "state": True }, "tan":1}
-    response = requests.post('http://127.0.0.1:8090/json-rpc', headers=headers, json=data)
-    addon.setSetting('state', 'true')
-
-def ambilight_bright_up():
-    bright = int(addon.getSetting('bright'))
-    if bright < 100:
-        bright = bright+10
-        data = {"command":"adjustment", "adjustment":{"brightness": bright }, "tan":1}
-        response = requests.post('http://127.0.0.1:8090/json-rpc', headers=headers, json=data)
-        addon.setSetting('bright', str(bright))
-        send_notification("Ambilight", "Jasność: "+str(bright))
-
-def ambilight_bright_down():
-    bright = int(addon.getSetting('bright'))
-    if bright > 10:
-        bright = bright-10
-        data = {"command":"adjustment", "adjustment":{"brightness": bright }, "tan":1}
-        response = requests.post('http://127.0.0.1:8090/json-rpc', headers=headers, json=data)
-        addon.setSetting('bright', str(bright))
-        send_notification("Ambilight", "Jasność: "+str(bright))
-
-def cpu():
-    send_notification("", "[B]CPU:[/B] $INFO[System.CPUUsage] [CR][B]Temperatura:[/B] $INFO[System.CPUTemperature]   [B]RAM:[/B] $INFO[System.memory(used.percent)] [B]Up:[/B] $INFO[System.Uptime]")
+def set_setting_and_notify(name, value, message=None):
+    """Ustawia wartość i wyświetla powiadomienie."""
+    addon.setSetting(name, str(value))
+    if message:
+        send_notification("Ambilight", message)
 
 def send_notification(komponent, message):
     xbmcgui.Dialog().notification(komponent, message, icon=icon)
 
-def get_setting(name):
-    return addon.getSetting(name)
+# Funkcje Ambilight
+def ambilight_turn_off():
+    send_request("componentstate", {"componentstate": {"component": "LEDDEVICE", "state": False}})
+    set_setting_and_notify('state', 'false')
 
-def set_setting(name, value):
-    addon.setSetting(name, value)
+def ambilight_turn_on():
+    send_request("componentstate", {"componentstate": {"component": "LEDDEVICE", "state": True}})
+    set_setting_and_notify('state', 'true')
+
+def adjust_brightness(change):
+    bright = int(addon.getSetting('bright')) + change
+    bright = max(0, min(100, bright))  # Ograniczenie do przedziału 0-100
+    send_request("adjustment", {"adjustment": {"brightness": bright}})
+    set_setting_and_notify('bright', bright, f"Jasność: {bright}")
+
+def ambilight_bright_up():
+    adjust_brightness(10)
+
+def ambilight_bright_down():
+    adjust_brightness(-10)
+
+def cpu():
+    """Wyświetla informacje o CPU i RAM."""
+    info = ("[B]CPU:[/B] $INFO[System.CPUUsage] [CR]"
+            "[B]Temperatura:[/B] $INFO[System.CPUTemperature] "
+            "[B]RAM:[/B] $INFO[System.memory(used.percent)] "
+            "[B]Up:[/B] $INFO[System.Uptime]")
+    send_notification("", info)
