90a215b42d1dbc219667a81a93241455abb3d450
notoco <36659110+notoco@users.noreply.github.com>
2025-01-12 19:42:41 +0100

    Revert "Update control.py"
    This reverts commit a3a07e2bd916b756a0579fdc9aed9cd5acb291a6.

--- script.notoco.helper/control.py
+++ script.notoco.helper/control.py
@@ -2,37 +2,25 @@ import xbmcaddon
 import xbmcgui
 import requests
 
-# Stałe
-HEADERS = {'Content-Type': 'application/json'}
-URL = 'http://127.0.0.1:8090/json-rpc'
+# Konfiguracja
+headers = {'Content-Type': 'application/json'}
 addon = xbmcaddon.Addon()
-ICON = addon.getAddonInfo('icon')
-
-def get_setting(name):
-    return addon.getSetting(name)
-
-def set_setting(name, value):
-    addon.setSetting(name, value)
+icon = addon.getAddonInfo('icon')
 
 def send_request(command, payload):
     """Wysyła żądanie do serwera Ambilight."""
+    url = 'http://127.0.0.1:8090/json-rpc'
     data = {"command": command, **payload, "tan": 1}
-    try:
-        response = requests.post(URL, headers=HEADERS, json=data)
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        xbmcgui.Dialog().notification("Błąd", str(e), icon=ICON)
-        return None
+    requests.post(url, headers=headers, json=data)
 
 def set_setting_and_notify(name, value, message=None):
     """Ustawia wartość i wyświetla powiadomienie."""
-    set_setting(name, str(value))
+    addon.setSetting(name, str(value))
     if message:
         send_notification("Ambilight", message)
 
 def send_notification(komponent, message):
-    xbmcgui.Dialog().notification(komponent, message, icon=ICON)
+    xbmcgui.Dialog().notification(komponent, message, icon=icon)
 
 # Funkcje Ambilight
 def ambilight_turn_off():
@@ -44,7 +32,7 @@ def ambilight_turn_on():
     set_setting_and_notify('state', 'true')
 
 def adjust_brightness(change):
-    bright = int(get_setting('bright')) + change
+    bright = int(addon.getSetting('bright')) + change
     bright = max(0, min(100, bright))  # Ograniczenie do przedziału 0-100
     send_request("adjustment", {"adjustment": {"brightness": bright}})
     set_setting_and_notify('bright', bright, f"Jasność: {bright}")
